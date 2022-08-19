058fbc164ec8d8661ed0fa8cfbad0b7fcca9404d
Yann Figueiredo <yann.fabricio@hotmail.com>
2022-08-19 11:24:43 -0300

    Iniciado projeto backend

---
+++ backend/src/main/java/com/yannfigueiredo/petsarea/PetsareaApplication.java
@@ -0,0 +1,13 @@
+package com.yannfigueiredo.petsarea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetsareaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetsareaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/yannfigueiredo/petsarea/PetsareaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yannfigueiredo.petsarea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetsareaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
